1cf1e783bc9a8db85ab08033e1b74620f5c9f314
b3koslev <b3koslev@SZT1-19-HP>
2026-01-26 09:28:35 +0100

    newEvent function created

--- calendar/Program.cs
+++ calendar/Program.cs
@@ -8,54 +8,6 @@ namespace calendar
 {
     internal class Program
     {
-        static void PrintCalendar()
-        {
-
-        }
-
-        static void NewEvent()
-        {
-
-        }
-
-        static void ViewNextEvent()
-        {
-        }
-
-        static void Exit()
-        {
-
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("1. Naptár megjelenítése");
-            Console.WriteLine("2. Új esemény hozzáadása");
-            Console.WriteLine("3. Legközelebbi esemény megjelenítése");
-            Console.WriteLine("4. Kilépés");
-
-            Console.WriteLine();
-
-            Console.Write("Válassz egy opciót (1-4): ");
-
-            int choice = Convert.ToInt32(Console.ReadLine());
-            if (choice == 1)
-            {
-                PrintCalendar();
-            }
-            else if (choice == 2)
-            {
-                NewEvent();
-            }
-            else if (choice == 3)
-            {
-                ViewNextEvent();
-            }
-            else if (choice == 4)
-            {
-                Exit();
-            }
-        }
-
         public struct Event
         {
             public string Parent;
@@ -68,6 +20,106 @@ namespace calendar
                 Date = date;
                 Duration = duration;
             }
+        }
+
+        static void PrintCalendar()
+        {
+
+        }
+
+        static Event NewEvent()
+        {
+            Console.Clear();
+
+            string parent = string.Empty;
+            DateTime date;
+            int duration;
+
+            do
+            {
+                Console.WriteLine("Add meg a felhasználót (anya/apa): ");
+                parent = Console.ReadLine();
+                parent = parent.ToLower();
+                if (parent != "anya" && parent != "apa")
+                {
+                    Console.WriteLine("Hibás felhasználó! Kérlek, próbáld újra.");
+                }
+            } while (parent != "anya" && parent != "apa");
+
+            do
+            {
+                Console.WriteLine("Add meg az esemény dátumát (ÉÉÉÉ-HH-NN ÓÓ:PP): ");
+                date = Convert.ToDateTime(Console.ReadLine());  
+                if (date.Year != 2028 || date.Month != 02 || date.Hour < 8 || date.Hour > 20)
+                {
+                    Console.WriteLine("Hibás dátum! Kérlek, próbáld újra.");
+                }
+            } while (date.Year != 2028 || date.Month != 02 || date.Hour < 8 || date.Hour > 20);
+
+            do
+            {
+                Console.WriteLine("Add meg az esemény időtartamát (perc): ");
+                duration = Convert.ToInt32(Console.ReadLine());
+                if (duration < 30 || duration > 120)
+                {
+                    Console.WriteLine("Hibás időtartam! Kérlek, próbáld újra.");
+                }
+            } while (duration < 30 || duration > 120);
+
+            Event new_event = new Event(parent, date, duration);
+
+            return new_event;
+        }
+
+        static void ViewNextEvent()
+        {
+        }
+
+        static bool Exit()
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================================================="); 
+            Console.WriteLine("\tNaptár alkalmazás - Február hónap");
+            Console.WriteLine("=================================================");
+            Console.WriteLine();
+
+            bool endOfCalendar = false;
+            List<Event> events = new List<Event>();
+
+            do
+            {
+                Console.WriteLine("1. Naptár megjelenítése");
+                Console.WriteLine("2. Új esemény hozzáadása");
+                Console.WriteLine("3. Legközelebbi esemény megjelenítése");
+                Console.WriteLine("4. Kilépés");
+
+                Console.WriteLine();
+
+                Console.Write("Válassz egy opciót (1-4): ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    PrintCalendar();
+                }
+                else if (choice == 2)
+                {
+                    events.Add(NewEvent());
+                }
+                else if (choice == 3)
+                {
+                    ViewNextEvent();
+                }
+                else if (choice == 4)
+                {
+                    endOfCalendar = Exit();
+                    Console.WriteLine("Kilépés...");
+                }
+            } while (false);
         }
     }
 }
