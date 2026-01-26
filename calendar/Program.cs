da7806ea029f0dd8eb89d0d914cb5b1b297be0fe
b3koslev <b3koslev@SZT1-19-HP>
2026-01-26 08:49:09 +0100

    options created

--- calendar/Program.cs
+++ calendar/Program.cs
@@ -8,8 +8,66 @@ namespace calendar
 {
     internal class Program
     {
+        static void PrintCalendar()
+        {
+
+        }
+
+        static void NewEvent()
+        {
+
+        }
+
+        static void ViewNextEvent()
+        {
+        }
+
+        static void Exit()
+        {
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("1. Naptár megjelenítése");
+            Console.WriteLine("2. Új esemény hozzáadása");
+            Console.WriteLine("3. Legközelebbi esemény megjelenítése");
+            Console.WriteLine("4. Kilépés");
+
+            Console.WriteLine();
+
+            Console.Write("Válassz egy opciót (1-4): ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                PrintCalendar();
+            }
+            else if (choice == 2)
+            {
+                NewEvent();
+            }
+            else if (choice == 3)
+            {
+                ViewNextEvent();
+            }
+            else if (choice == 4)
+            {
+                Exit();
+            }
+        }
+
+        public struct Event
+        {
+            public string Parent;
+            public DateTime Date;
+            public int Duration;
+
+            public Event(string parent, DateTime date, int duration)
+            {
+                Parent = parent;
+                Date = date;
+                Duration = duration;
+            }
         }
     }
 }
